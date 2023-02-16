95fe91cace8d35e043a4ca66165a26324c110b89
Elena Gradovich <volozhinskaya@gmail.com>
2023-02-16 12:41:13 -0400

    VUE/8_refactor and add websocket

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,33 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <!-- <div
-      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div> -->
     <div class="container">
+      <div class="w-full my-4"></div>
       <section>
         <div class="flex">
           <div class="max-w-xs">
             <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
+              >Ticker</label
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
-                v-model="inputCurrency"
-                v-on:keydown.enter="onAddValue"
+                v-model="ticker"
+                @keydown.enter="add"
                 v-on:input="getAutocomplete"
                 type="text"
                 name="wallet"
                 id="wallet"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
+                placeholder="Example DOGE"
               />
             </div>
             <div
-              v-if="autocompleteList.length"
+              v-if="autocompleteList?.length"
               class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
             >
               <span
                 v-for="autocompleteItem in autocompleteList"
                 v-bind:key="autocompleteItem"
-                v-on:click="inputCurrency = autocompleteItem"
+                v-on:click="ticker = autocompleteItem"
                 class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
               >
                 {{ autocompleteItem }}
@@ -61,7 +38,7 @@
         </div>
         <button
           v-bind:title="message"
-          @click="onAddValue"
+          @click="add"
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -81,62 +58,49 @@
           Add
         </button>
       </section>
-      <template v-if="tickers.length">
+
+      <template v-if="tickers?.length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <div>
-          <!-- Filter -->
-          <div
-            class="my-4 inline-flex items-center py-0 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Filter
-            <input
-              @input="page = 1"
-              v-model="inputFilter"
-              type="text"
-              class="text-black"
-            />
-          </div>
-
-          <!-- Pagination -->
+          Page {{ page }}
           <button
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            data-button-prev
-            v-on:click.stop="page > 1 ? page-- : page"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
           >
             Prev
           </button>
-          {{ page }}
           <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="page = page + 1"
             v-if="hasNextPage"
-            v-on:click.stop="hasNextPage ? page++ : page"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled"
-            data-button-next
           >
             Next
           </button>
+          <div>Filter: <input v-model="filter" /></div>
         </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="{ currency, price } in paginatedTickers"
-            v-bind:key="currency"
+            v-for="t in paginatedTickers"
+            :key="t.currency"
+            @click="select(t)"
             :class="{
-              'border-4 border-purple-800':
-                selectedTicker?.currency === currency,
+              'border-4': selectedTicker === t,
             }"
-            @click="selectedTicker = { currency, price }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ currency }} - {{ defaultCurrency }}
+                {{ t.currency }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ price }}
+                {{ formatPrice(t.price) }}
               </dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click.stop="onDeleteCard(currency)"
+              @click.stop="handleDelete(t)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -159,7 +123,7 @@
       </template>
       <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.currency }} - {{ defaultCurrency }}
+          {{ selectedTicker.currency }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
@@ -202,24 +166,31 @@
 </template>
 
 <script>
+import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+
 export default {
   name: "App",
+
   data() {
     return {
-      inputCurrency: null,
-      inputFilter: "", //filter
+      ticker: "",
+      filter: "",
+
       tickers: [],
       currencies: [],
       selectedTicker: null,
       message: `"Вы загрузили эту страницу:" ${new Date().toLocaleString()}`,
-      defaultCurrency: "USD",
+      graph: [],
+
+      page: 1,
       addFieldErrorEL: null,
       autocompleteList: [],
-      page: 1,
-      tickersPerPage: 6,
-      graph: [],
     };
   },
+  mounted: function () {
+    this.addFieldErrorEL = document.querySelector("[data-add-error]");
+  },
+
   created() {
     fetch("https://min-api.cryptocompare.com/data/all/coinlist?summary=true")
       .then((response) => response.json())
@@ -232,85 +203,86 @@ export default {
       this.tickers = JSON.parse(tickerData);
       this.tickers.forEach((item) => this.subscribeToUpdate(item.currency));
     }
-
     const windowData = Object.fromEntries(
-      new URL(window.location).searchParams?.entries()
+      new URL(window.location).searchParams.entries()
     );
 
-    if (windowData) {
-      this.inputFilter = windowData?.filter;
-      this.page = windowData?.page;
-    }
-  },
-  mounted: function () {
-    this.addFieldErrorEL = document.querySelector("[data-add-error]");
-    this.nextPageButton = document.querySelector("[data-button-next]");
-    this.prevPageButton = document.querySelector("[data-button-prev]");
-  },
-  watch: {
-    paginatedTickers() {
-      if (this.paginatedTickers.length === 0 && +this.page > 0) {
-        this.page -= 1;
+    const VALID_KEYS = ["filter", "page"];
+
+    VALID_KEYS.forEach((key) => {
+      if (windowData[key]) {
+        this[key] = windowData[key];
       }
-    },
-    selectedTicker() {
-      this.graph = []; //сброс графика
+    });
+
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach((ticker) => {
+        subscribeToTicker(ticker.currency, (newPrice) =>
+          this.updateTicker(ticker.currency, newPrice)
+        );
+      });
+    }
+
+    setInterval(this.updateTickers, 5000);
+  },
+
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6;
     },
 
-    pageStateOptions(value) {
-      window.history.pushState(
-        null,
-        document.title,
-        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
-      );
-    },
-  },
-  computed: {
-    pageStateOptions() {
-      // when changed inputFilter and page pageStateOptions computed for watch
-      return {
-        filter: this.inputFilter,
-        page: this.page,
-      };
-    },
-    startIndex() {
-      return this.tickersPerPage * (this.page - 1);
-    },
     endIndex() {
-      return this.tickersPerPage * this.page;
+      return this.page * 6;
     },
+
     filteredTickers() {
-      const filteredTickers = this.tickers.filter((ticker) =>
-        ticker.currency.toLowerCase().includes(this.inputFilter.toLowerCase())
+      return this.tickers.filter((ticker) =>
+        ticker.currency?.toLowerCase().includes(this.filter?.toLowerCase())
       );
-      return filteredTickers;
     },
+
     paginatedTickers() {
       return this.filteredTickers.slice(this.startIndex, this.endIndex);
     },
+
     hasNextPage() {
       return this.filteredTickers.length > this.endIndex;
     },
+
     normalizedGraph() {
       const maxValue = Math.max(...this.graph);
       const minValue = Math.min(...this.graph);
 
       if (maxValue === minValue) {
-        return this.graph?.map(() => 50);
+        return this.graph.map(() => 50);
       }
-      return this.graph?.map((price) => {
-        5 + (price - minValue) / (maxValue - minValue);
-      });
+
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+
+    pageStateOptions() {
+      return {
+        filter: this.filter,
+        page: this.page,
+      };
     },
   },
+
   methods: {
     checkTicker(ticker, tickers) {
       if (tickers.length) {
         const isExist =
           tickers.length &&
-          [...tickers?.map((item) => item.currency.toLowerCase())]?.includes(
-            ticker.toLowerCase()
-          );
+          [
+            ...tickers?.map((item) => {
+              return item.currency.toLowerCase();
+            }),
+          ]?.includes(ticker.toLowerCase());
         return isExist;
       }
       return false;
@@ -322,9 +294,9 @@ export default {
       if (el) el.innerText = "";
     },
     getAutocomplete() {
-      if (this.currencies.length && this.inputCurrency !== "") {
+      if (this.currencies.length && this.ticker !== "") {
         const newList = this.currencies.filter((item) =>
-          item.Symbol.toLowerCase().includes(this.inputCurrency.toLowerCase())
+          item.Symbol.toLowerCase().includes(this.ticker.toLowerCase())
         );
 
         if (newList.length) {
@@ -336,66 +308,93 @@ export default {
 
         this.autocompleteList = [];
       }
-      if (this.inputCurrency === "") {
+      if (this.ticker === "") {
         this.autocompleteList = [];
       }
     },
-    subscribeToUpdate(ticker) {
-      const API_KEY =
-        "5e8354e068a3885f5bf11b367f32efa47041c0d0abb5594620d22a04e61c1c7a";
-      const API = `https://min-api.cryptocompare.com/data/price?fsym=${ticker}&tsyms=USD,JPY,EUR&api_key=${API_KEY}`;
-      setInterval(async () => {
-        await fetch(API)
-          .then((response) => response.json())
-          .then((data) => {
-            this.tickers.forEach((item) => {
-              if (item.currency === ticker)
-                item.price = data[this.defaultCurrency];
-            });
+    updateTicker(tickerCurrency, price) {
+      this.tickers
+        .filter((t) => t.currency === tickerCurrency)
+        .forEach((t) => {
+          if (t === this.selectedTicker) {
+            this.graph.push(price);
+          }
+          t.price = price;
+        });
+    },
 
-            if (this.selectedTicker?.currency === ticker) {
-              this.graph.push(data[this.defaultCurrency]);
-            }
-          });
-      }, 5000);
+    formatPrice(price) {
+      if (price === "-") {
+        return price;
+      }
+      return price > 1 ? price.toFixed(2) : price.toPrecision(2);
     },
-    resetFilter() {
-      this.inputFilter = "";
-    },
-    resetPage() {
-      this.page = 1;
-    },
-    onAddValue() {
-      this.resetFilter();
-      this.resetPage();
-      const isTicker = this.checkTicker(this.inputCurrency, this.tickers);
+
+    add() {
+      const isTicker = this.checkTicker(this.ticker, this.tickers);
       if (isTicker) {
         this.showError("Такой тикер уже добавлен", this.addFieldErrorEL);
-        this.inputCurrency = "";
         return;
       }
-      if (this.inputCurrency) {
-        const newTicker = {
-          currency: this.inputCurrency,
-          price: "-",
-        };
-        this.tickers = [...this.tickers, newTicker];
-        localStorage.setItem("currencyList", JSON.stringify(this.tickers));
-        this.subscribeToUpdate(newTicker.currency);
-        this.inputCurrency = "";
-      }
+      const currentTicker = {
+        currency: this.ticker.toUpperCase(),
+        price: "-",
+      };
+
+      this.tickers = [...this.tickers, currentTicker];
+      this.ticker = "";
+      this.filter = "";
+      subscribeToTicker(currentTicker.currency, (newPrice) =>
+        this.updateTicker(currentTicker.currency, newPrice)
+      );
+      this.autocompleteList = [];
+      this.hideError(this.showError(this.addFieldErrorEL));
     },
-    onDeleteCard(currency) {
-      this.tickers = this.tickers.filter((item) => {
-        return item.currency !== currency;
-      });
-      localStorage.setItem("currencyList", JSON.stringify(this.tickers));
-    },
+
     select(ticker) {
       this.selectedTicker = ticker;
+    },
+
+    handleDelete(tickerToRemove) {
+      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      if (this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null;
+      }
+      unsubscribeFromTicker(tickerToRemove.currency);
+    },
+  },
+
+  watch: {
+    ticker() {
+      this.autocompleteList = [];
+    },
+
+    selectedTicker() {
+      this.graph = [];
+    },
+
+    tickers(newValue, oldValue) {
+      console.log(newValue === oldValue);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+    },
+
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
+        this.page -= 1;
+      }
+    },
+
+    filter() {
+      this.page = 1;
+    },
+
+    pageStateOptions(value) {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
+      );
     },
   },
 };
 </script>
-
-<style></style>
